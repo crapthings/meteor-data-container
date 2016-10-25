4ee145c4eb7663de2cfb1fa3795285cbb4ac9079
crapthings <crapthings@gmail.com>
2016-10-25 15:14:28 +0800

    rls v0.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'crapthings:meteor-data-container',
   summary: 'Meteor Data Container',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/crapthings/meteor-data-container.git',
 })
 
--- src.js
+++ src.js
@@ -2,12 +2,13 @@ import { composeWithTracker as ComposeDataContainer } from 'react-komposer'
 
 import { map, mapValues, every, isFunction } from 'lodash'
 
-MeteorDataContainer = ({ sources, component, ...options }) => {
+MeteorDataContainerCache = new SubsManager()
 
+MeteorDataContainer = ({ sources, component, ...options }) => {
   const _data = sources.data
   const _subscriptions = sources.subscriptions
 
-  const Subscribe = !options.cache ? Meteor.subscribe : new SubsManager().subscribe
+  const Subscribe = options.cache ? MeteorDataContainerCache.subscribe.bind(MeteorDataContainerCache) : Meteor.subscribe
 
   function defaultTracker(props, onData) {
     const _loaded = _subscriptions ? map(_subscriptions, (val, key) => Subscribe(key, ...val).ready()) : [true]
